ae57c6bbfba208753bdbaa50d9efe82e216345d9
Kroid <kroid@yandex.ru>
2013-06-08 14:50:34 +0400

    create_app, run_app

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,23 @@
+import logging
 from flask import Flask
-from redis_session import RedisSessionInterfase
+from redis_session import RedisSessionInterface
 
-def create_app():
-	pass
+app = None
+app_run_args = {}
+
+def create_app(mode = "production"):
+	global app
+	app = Flask("application")
+	global app_run_args
+	app_run_args = {'port': 5000, 'host': '127.0.0.1'}
+
+	if mode == "production":
+		app.debug = False
+	elif mode == "dev":
+		app.debug = True
+	else:
+		logging.error("Did not recognize mode '%s'" % mode)
+
 
 def run_app():
-	pass
+	app.run(**app_run_args)
